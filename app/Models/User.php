e2280501dde6e69a388461b693d1168fff974bdc
Andy Haryoko <turnback2ubuntu@gmail.com>
2026-08-16 13:28:51 +0700

    fix: restore missing bracket in User model casts

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,8 +27,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+        ];
     }
-
     public function pendidikans()
     {
         return $this->hasMany(Pendidikan::class);
